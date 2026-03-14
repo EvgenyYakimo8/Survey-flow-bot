9bfde8bdc8bfbe7e09d71f18d6ec28b323d6f5ed
Якимов Евгений Сергеевич <yakimov_es@peleng.jsc.local>
2026-03-14 17:27:50 +0300

    test

---
+++ src/main/java/io/telegram/bot/infrastructure/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package io.telegram.bot.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
